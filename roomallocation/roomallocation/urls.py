8eaaa77d3702b617a3459a6f293da6a3bb725118
Thapelo Tsotetsi <mrtsotetsithapelo@gmail.com>
2015-06-03 00:22:07 +0200

    fix wrong table name

--- roomallocation/roomallocation/urls.py
+++ roomallocation/roomallocation/urls.py
@@ -17,5 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+
+    url(r'^floor/', include('rooms.urls')),
+#    url(r'^flat/', include('rooms.urls')),
     url(r'^admin/', include(admin.site.urls)),
+	
 ]
--- roomallocation/rooms/urls.py
+++ roomallocation/rooms/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns =[
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<flat_floor_id>[0-9]+)/$', views.floor, name='floors'),
-	url(r'^(?P<flat_floor_id>[0-9]+)/(?P<flat_room_id>[a-z0-9]+)/$', views.flat, name='flat'),
+	url(r'^(?P<flat_floor_id>[0-9]+)/(?P<flat_number>[a-z0-9]+)/$', views.flat, name='flat'),
 	
 ]
