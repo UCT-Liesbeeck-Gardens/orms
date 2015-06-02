4ea3a49b4a6a340da24074c69744e33c303878a6
Thapelo Tsotetsi <mrtsotetsithapelo@gmail.com>
2015-06-02 20:39:28 +0200

    render template given floors

--- roomallocation/rooms/views.py
+++ roomallocation/rooms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.template import RequestContext, loader
+
+from .models import Flat, Floor
+
 
 # Create your views here.
+def index(request):
+
+	residence_floors = Floor.objects.values_list('floor', flat=True)
+	template = loader.get_template('rooms/index.html')
+	context = RequestContext(request, {
+		'residence_floors': residence_floors,
+		})
+	return HttpResponse(template.render(context))
+
+def floor(request, flat_floor_id):
+	return HttpResponse("Display all available rooms at that floor.")
+
+def flat(request, flat_floor_id, flat_room_id):
+	return HttpResponse("Displays the specific flat/room")
