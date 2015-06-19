8e20895286926c04da5a53b2674820a62e718131
Thapelo Tsotetsi <mrtsotetsithapelo@gmail.com>
2015-06-19 13:31:12 +0200

    add application decline url pattern

--- roomallocation/roomallocation/urls.py
+++ roomallocation/roomallocation/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from rooms.views import login, logout, authenticate, applications, application_details, approve_application
+from rooms.views import login, logout, authenticate, applications, application_details, approve_application, decline_application
 
 urlpatterns = [
 
@@ -26,5 +26,6 @@ urlpatterns = [
     url(r'^authenticate/applications/$', applications),
     url(r'^authenticate/applications/(?P<flat_number>[a-z0-9]+)/(?P<student_number>[a-z0-9]+)/$', application_details),
     url(r'^authenticate/applications/(?P<flat_number>[a-z0-9]+)/(?P<student_number>[a-z0-9]+)/approve/$', approve_application),
+    url(r'^authenticate/applications/(?P<flat_number>[a-z0-9]+)/(?P<student_number>[a-z0-9]+)/decline/$', decline_application),    
     url(r'^logout/$', logout),
 ]
