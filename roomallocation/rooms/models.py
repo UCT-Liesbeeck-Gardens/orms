01016519216f4c210692f53218661e21af47ae7f
Thapelo Tsotetsi <mrtsotetsithapelo@gmail.com>
2015-06-21 12:47:05 +0200

    normalize models to avoid data dubplicate in a db

--- roomallocation/rooms/models.py
+++ roomallocation/rooms/models.py
@@ -1,57 +1,63 @@
 from django.db import models
 
 class Floor(models.Model):
-	floor = models.IntegerField(unique=True)
+	floor = models.IntegerField(max_length=4, unique=True)
 
 	def __unicode__(self):
 		return self.floor
 
+
+class FlatType(models.Model):
+	flat_type = models.CharField(max_length=20, unique=True)
+
+	def __unicode__(self):
+		return self.flat_type
+
+
 class Flat(models.Model):
 	"""building/flat model"""
 	flat_floor = models.ForeignKey(Floor)
-	flat_number = models.CharField(max_length=10)
-	room_number = models.CharField(max_length=10)
-	flat_type = models.CharField(max_length=10)
+	flat_type = models.ForeignKey(FlatType)
+	flat_number = models.CharField(max_length=10, unique=True)
 	additional_info = models.CharField(max_length=100)
 
 	def __unicode__(self):
 		return self.flat_number
 
-class Application(models.Model):
-	"""student applicants model"""
-	flat_number = models.CharField(max_length=10)
-	name = models.CharField(max_length=20)
-	student_number = models.CharField(max_length=10)
+class Student(models.Model):
+	"""student model"""
+	student_number	= models.CharField(max_length=20, unique=True)
+	name = models.CharField(max_length=30)
+	gender = models.CharField(max_length=10)
 	mobile_number = models.CharField(max_length=20)
 	email_address = models.CharField(max_length=100)
-	gender = models.CharField(max_length=10)
+
+	def __unicode__(sefl):
+		return self.student_number
+
+class Application(models.Model):
+	"""student applicants model"""
+	flat_number = models.ForeignKey(Flat)
+	student = models.ForeignKey(Student)
 	date_of_application = models.DateTimeField('date applied')
 
 	def __unicode__(self):
-		return self.student_number
+		return self.student
 
 class Approval(models.Model):
 	"""applications approval model"""
-	flat_number = models.CharField(max_length=10)
-	name = models.CharField(max_length=20)
-	student_number = models.CharField(max_length=10)
-	mobile_number = models.CharField(max_length=20)
-	email_address = models.CharField(max_length=100)
-	gender = models.CharField(max_length=10)
+	flat_number = models.ForeignKey(Flat)
+	student = models.ForeignKey(Student)
 	date_of_approval = models.DateTimeField('date approved')
 
 	def __unicode__(self):
-		return self.student_number
+		return self.student
 
 class Decline(models.Model):
 	"""applications declines model"""
-	flat_number = models.CharField(max_length=10)
-	name = models.CharField(max_length=20)
-	student_number = models.CharField(max_length=10)
-	mobile_number = models.CharField(max_length=20)
-	email_address = models.CharField(max_length=100)
-	gender = models.CharField(max_length=10)
-	date_of_decline = models.DateTimeField('date declined')
+	flat_number = models.ForeignKey(Flat)
+	student = models.ForeignKey(Student)
+	date_of_approval = models.DateTimeField('date declined')
 
 	def __unicode__(self):
-		return self.student_number
+		return self.student
